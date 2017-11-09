8ac3af930e0921110e1c92536f61e3ed3b1c243b
Pavel Annin <anninpavel2014@gmail.com>
2017-11-10 00:24:58 +0300

    chore (lesson-03) add screen layout

---
+++ lesson03/app/src/main/java/ru/annin/penzgtulesson03/App.kt
@@ -0,0 +1,17 @@
+package ru.annin.penzgtulesson03
+
+import android.app.Application
+import android.support.v7.app.AppCompatDelegate
+
+/**
+ * Класс приложения.
+ *
+ * @author Pavel Annin.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+    }
+}
--- lesson03/app/src/main/java/ru/annin/penzgtulesson03/ui/activity/MainActivity.kt
+++ lesson03/app/src/main/java/ru/annin/penzgtulesson03/ui/activity/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.annin.penzgtulesson03.ui.activity
 
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import ru.annin.penzgtulesson03.R
 
+/**
+ * Главный экран.
+ *
+ * @author Pavel Annin.
+ */
 class MainActivity : AppCompatActivity() {
 
     companion object {
@@ -15,5 +24,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private class ViewDelegate(private val vRoot: View) {
+
+        // View's
+        private val edtUrl by lazy { vRoot.findViewById<EditText>(R.id.edt_url) }
+        private val ibtnSearch by lazy { vRoot.findViewById<ImageButton>(R.id.ibtn_search) }
+        private val ivPhoto by lazy { vRoot.findViewById<ImageView>(R.id.iv_photo) }
+        private val ibtnLike by lazy { vRoot.findViewById<ImageButton>(R.id.ibtn_like) }
+        private val ibtnDislike by lazy { vRoot.findViewById<ImageButton>(R.id.ibtn_dislike) }
+
+        // Listener's
+        var onSearchClick: ((url: CharSequence) -> Unit)? = null
+        var onLikeClick: (() -> Unit)? = null
+        var onDislikeClick: (() -> Unit)? = null
+
+        init {
+            ibtnSearch.setOnClickListener { onSearchClick?.invoke(edtUrl.text) }
+            ibtnLike.setOnClickListener { onLikeClick?.invoke() }
+            ibtnDislike.setOnClickListener { onDislikeClick?.invoke() }
+        }
     }
 }
